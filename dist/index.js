edcd6516bfee4f960a98416717d584eb2cd4db08
Abhidb2007 <abhishekdb2210@gmail.com>
2025-10-27 15:25:42 +0530

    code

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+import { WebSocketServer } from "ws";
+const wss = new WebSocketServer({ port: 8080 });
+wss.on("connection", (socket) => {
+    console.log("user connected");
+    socket.send("hello hi");
+    setInterval(() => {
+        socket.send("current price of solona is" + Math.random());
+    }, 5000);
+    socket.on("message", (message) => {
+        console.log("Received:", message.toString());
+    });
+});
+//# sourceMappingURL=index.js.map
