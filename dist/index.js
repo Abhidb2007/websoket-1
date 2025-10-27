975d42e65aac6b9d620bfbb4a31943242f09cd7f
Abhidb2007 <abhishekdb2210@gmail.com>
2025-10-27 19:42:21 +0530

    code

--- dist/index.js
+++ dist/index.js
@@ -2,12 +2,10 @@ import { WebSocketServer } from "ws";
 const wss = new WebSocketServer({ port: 8080 });
 wss.on("connection", (socket) => {
     console.log("user connected");
-    socket.send("hello hi");
-    setInterval(() => {
-        socket.send("current price of solona is" + Math.random());
-    }, 5000);
     socket.on("message", (e) => {
-        console.log(e.toString());
+        if (e.toString() === "ping") {
+            socket.send("pong");
+        }
     });
 });
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,13 @@ const wss = new WebSocketServer({ port: 8080 });
 
 wss.on("connection", (socket) => {
   console.log("user connected");
-  socket.send("hello hi");
-  setInterval(()=>{
-    socket.send("current price of solona is"+Math.random());
-  },5000)
+  
 
   socket.on("message", (e) => {
     console.log(e.toString());
+    console.log(e.toString()=== "ping");
+  if(e.toString() === "ping") {
+    socket.send("pong");
+  }
   });
 }); 
